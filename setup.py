925696b09616e01e803d29af190850efae622ba8
a-r-r-o-w <avs050602@gmail.com>
2023-08-07 18:45:25 +0530

    add setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stablefused",
+    version="0.1.0",
+    description="StableFused",
+    author="Aryan V S",
+    author_email="contact.aryanvs+stablefused@gmail.com",
+    python_requires=">=3.6.0",
+    packages=find_packages(),
+    install_requires=[
+        "accelerate",
+        "diffusers==0.19.3",
+        "torch==2.0.1",
+        "transformers==4.31.0",
+        "scipy",
+        "ftfy",
+        "ipython",
+        "matplotlib",
+        "pytest>=7.4.0",
+    ],
+)
